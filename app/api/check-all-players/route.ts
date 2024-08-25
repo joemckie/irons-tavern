db6a057b66665a143f36bb949c6b6774da13c5e3
Joe McKie <joe@joemck.ie>
2024-08-25 14:03:06 +0100

    feat: refresh cache after all players are checked

--- app/api/check-all-players/route.ts
+++ app/api/check-all-players/route.ts
@@ -13,9 +13,12 @@ export async function POST() {
 
   // Filter players that have been checked within the last day.
   // This reduces the amount of requests significantly, due to
-  // players using the XP Updater plugin on RuneLite
+  // players using the XP Updater plugin on RuneLite.
+  // Also filter players not on hiscores as these cannot be checked.
   const filteredPlayers = Object.values(players.data.memberlist).filter(
-    (player) => differenceInDays(Date.now(), player.last_checked) > 1,
+    (player) =>
+      player.on_hiscores &&
+      differenceInDays(Date.now(), player.last_checked) > 1,
   );
 
   const requests = filteredPlayers.map((player, i) => ({
@@ -24,6 +27,12 @@ export async function POST() {
     url: `${constants.temple.baseUrl}/php/add_datapoint.php?player=${encodeURI(player.player)}&_delay=${i * 6}`,
     method: 'GET',
   }));
+
+  // After all checks have been completed, clear the cache to ensure the new data is displayed
+  requests.push({
+    url: `${constants.publicUrl}/api/revalidate-path?path=/&_delay=${requests.length * 6}`,
+    method: 'GET',
+  });
 
   const queueResponse = await fetch(
     `${constants.zeplo.url}/bulk?_token=${constants.zeplo.apiKey}`,
