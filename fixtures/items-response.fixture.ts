83a5eca7fc97c728b274379d42079945f5ff9ff5
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-11 20:57:36 +0100

    chore: add more items

--- fixtures/items-response.fixture.ts
+++ fixtures/items-response.fixture.ts
@@ -7,7 +7,7 @@ export const itemsResponseFixture: ItemsResponse = {
       {
         image:
           'https://oldschool.runescape.wiki/images/Abyssal_bludgeon_detail.png?dd634',
-        name: 'Abyssal Bludgeon',
+        name: 'Abyssal bludgeon',
         points: 60,
         requiredItems: [
           {
@@ -21,6 +21,377 @@ export const itemsResponseFixture: ItemsResponse = {
           {
             amount: 1,
             clogName: 'Bludgeon spine',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Abyssal dagger',
+        points: 30,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Abyssal dagger',
+          },
+        ],
+      },
+    ],
+  },
+  'Alchemical Hydra': {
+    image: '',
+    items: [
+      {
+        image: '',
+        name: 'Brimstone ring',
+        points: 30,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: "Hydra's eye",
+          },
+          {
+            amount: 1,
+            clogName: "Hydra's fang",
+          },
+          {
+            amount: 1,
+            clogName: "Hydra's heart",
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Hydra tail',
+        points: 20,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Hydra tail',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Hydra leather',
+        points: 60,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Hydra leather',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: "Hydra's claw",
+        points: 120,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: "Hydra's claw",
+          },
+        ],
+      },
+    ],
+  },
+  Araxxor: {
+    image: '',
+    items: [
+      {
+        image: '',
+        name: 'Coagulated venom',
+        points: 10,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Coagulated venom',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Noxious blade',
+        points: 30,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Noxious blade',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Noxious point',
+        points: 30,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Noxious point',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Noxious pommel',
+        points: 30,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Noxious pommel',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Araxyte fang',
+        points: 60,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Araxyte fang',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Amulet of rancour (s)',
+        points: 60,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Noxious blade',
+          },
+          {
+            amount: 1,
+            clogName: 'Noxious point',
+          },
+          {
+            amount: 1,
+            clogName: 'Noxious pommel',
+          },
+          {
+            amount: 1,
+            clogName: 'Areana boots',
+          },
+          {
+            amount: 1,
+            clogName: 'Araxyte head',
+          },
+          {
+            amount: 1,
+            clogName: 'Zenyte shard',
+          },
+          {
+            amount: 1,
+            clogName: 'Rax',
+          },
+        ],
+      },
+    ],
+  },
+  Cerberus: {
+    image: '',
+    items: [
+      {
+        image: '',
+        name: 'Primordial crystal',
+        points: 60,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Primordial crystal',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Pegasian crystal',
+        points: 40,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Pegasian crystal',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Eternal crystal',
+        points: 30,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Eternal crystal',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Smouldering stone',
+        points: 60,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Smouldering stone',
+          },
+        ],
+      },
+    ],
+  },
+  'Chambers of Xeric': {
+    image: '',
+    items: [
+      {
+        image: '',
+        name: 'Dexterous prayer scroll',
+        points: 80,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Dexterous prayer scroll',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Arcane prayer scroll',
+        points: 50,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Arcane prayer scroll',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Twisted buckler',
+        points: 80,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Twisted buckler',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Dragon hunter crossbow',
+        points: 150,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Dragon hunter crossbow',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: "Dinh's bulwark",
+        points: 80,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: "Dinh's bulwark",
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Ancestral hat',
+        points: 120,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Ancestral hat',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Ancestral robe top',
+        points: 120,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Ancestral robe top',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Ancestral robe bottom',
+        points: 120,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Ancestral robe bottom',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Dragon claws',
+        points: 150,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Dragon claws',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Elder maul',
+        points: 250,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Elder maul',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Kodai insignia',
+        points: 300,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Kodai insignia',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Twisted bow',
+        points: 400,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Twisted bow',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Twisted ancestral colour kit',
+        points: 80,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Twisted ancestral colour kit',
+          },
+        ],
+      },
+      {
+        image: '',
+        name: 'Metamorphic dust',
+        points: 300,
+        requiredItems: [
+          {
+            amount: 1,
+            clogName: 'Metamorphic dust',
           },
         ],
       },
