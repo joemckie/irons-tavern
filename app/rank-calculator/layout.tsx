9d22bd6c6016a34c2d5635c02a8e12647525d2b4
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-16 23:54:24 +0100

    chore: move tabs to their own pages

--- app/rank-calculator/layout.tsx
+++ app/rank-calculator/layout.tsx
@@ -1,7 +1,55 @@
-import { Theme, ThemePanel } from '@radix-ui/themes';
-import { PropsWithChildren } from 'react';
+'use client';
+
+import '@radix-ui/themes/styles.css';
+import { PropsWithChildren, useEffect, useRef, useState } from 'react';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { Flex, Grid, TabNav, Theme, ThemePanel } from '@radix-ui/themes';
+import { AchievementDiaryMap } from '@/types/rank-calculator';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Sidebar } from './components/sidebar';
+import { Navigation } from './components/navigation';
+import { PlayerDataProvider } from './contexts/player-data-context';
+
+interface FormData {
+  achievementDiaries: AchievementDiaryMap;
+  playerName: string;
+  items: Record<string, boolean>;
+}
 
 export default function RankCalculatorLayout({ children }: PropsWithChildren) {
+  const methods = useForm<FormData>({
+    defaultValues: {
+      achievementDiaries: {},
+      items: {},
+    },
+  });
+
+  const pathname = usePathname();
+  const navRef = useRef<HTMLElement>(null);
+  const tabsRef = useRef<HTMLDivElement>(null);
+  const [navHeight, setNavHeight] = useState<number>(54);
+  const [tabsHeight, setTabsHeight] = useState<number>(38);
+  const pageHeightCss = navHeight
+    ? `calc(100vh - ${navHeight}px - ${tabsHeight}px)`
+    : '100vh';
+
+  useEffect(() => {
+    if (navRef.current) {
+      setNavHeight(navRef.current.offsetHeight);
+    }
+  }, [navRef]);
+
+  useEffect(() => {
+    if (tabsRef.current) {
+      setTabsHeight(tabsRef.current.offsetHeight);
+    }
+  }, [tabsRef]);
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
   return (
     <Theme
       accentColor="iris"
@@ -9,7 +57,46 @@ export default function RankCalculatorLayout({ children }: PropsWithChildren) {
       panelBackground="solid"
       scaling="95%"
     >
-      {children}
+      <FormProvider {...methods}>
+        <PlayerDataProvider>
+          <form onSubmit={methods.handleSubmit(onSubmit)}>
+            <Grid
+              areas="
+              'nav nav'
+              'sidebar tabs'
+              '. main'
+            "
+              columns="[sidebar] minmax(200px, 1fr) [main] minmax(0, 3fr)"
+            >
+              <Navigation ref={navRef} />
+              <Sidebar />
+              <Flex
+                justify="center"
+                gridArea="tabs"
+                direction="column"
+                ref={tabsRef}
+              >
+                <TabNav.Root>
+                  <TabNav.Link active={pathname === '/rank-calculator'} asChild>
+                    <Link href="/rank-calculator">Overview</Link>
+                  </TabNav.Link>
+                  <TabNav.Link
+                    active={pathname === '/rank-calculator/notable-items'}
+                    asChild
+                  >
+                    <Link href="/rank-calculator/notable-items">
+                      Notable items
+                    </Link>
+                  </TabNav.Link>
+                </TabNav.Root>
+              </Flex>
+              <Flex gridArea="main" pl="3" height={pageHeightCss}>
+                {children}
+              </Flex>
+            </Grid>
+          </form>
+        </PlayerDataProvider>
+      </FormProvider>
       <ThemePanel defaultOpen={false} />
     </Theme>
   );
---
+++ app/rank-calculator/notable-items/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { ItemList } from '../components/item-list';
+
+export default function RankCalculatorNotableItems() {
+  return <ItemList />;
+}
