093037f38827e85ba808877e149ff5e50fd086c5
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-17 15:34:35 +0100

    chore: refactor

--- app/rank-calculator/notable-items/page.tsx
+++ app/rank-calculator/notable-items/page.tsx
@@ -1,7 +1,40 @@
 'use client';
 
-import { ItemList } from '../components/item-list';
+import { VariableSizeList } from 'react-window';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import { useDynamicItemSize } from '../hooks/use-dynamic-item-size';
+import { useGetItems } from '../hooks/use-get-items';
+import { ListItem } from '../components/list-item';
 
 export default function RankCalculatorNotableItems() {
-  return <ItemList />;
+  const { getSize, listRef, resetAfterIndex, setSize } = useDynamicItemSize();
+  const { data: categories } = useGetItems();
+
+  return (
+    <AutoSizer
+      onResize={() => {
+        resetAfterIndex(0, true);
+      }}
+    >
+      {({ height, width }) => (
+        <VariableSizeList
+          ref={listRef}
+          itemData={categories}
+          itemCount={categories.length}
+          height={height}
+          width={width}
+          itemSize={getSize}
+        >
+          {({ index, style }) => (
+            <ListItem
+              data={categories}
+              index={index}
+              style={style}
+              setSize={setSize}
+            />
+          )}
+        </VariableSizeList>
+      )}
+    </AutoSizer>
+  );
 }
