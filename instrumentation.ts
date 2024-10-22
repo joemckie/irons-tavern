69f567d2faa144e99a0a8d8e0cad22a9a701f72f
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-22 20:52:36 +0100

    fix: only load mocks in local env

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,6 @@
 export async function register() {
   if (
+    process.env.NODE_ENV === 'development' &&
     process.env.NEXT_RUNTIME === 'nodejs' &&
     process.env.NEXT_PUBLIC_API_MOCKING === 'enabled'
   ) {
