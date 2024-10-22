0834731e7db639d971f3991654fabb5d1ffa990d
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-22 22:47:56 +0100

    chore: compress

--- app/rank-calculator/hooks/use-point-calculator.ts
+++ app/rank-calculator/hooks/use-point-calculator.ts
@@ -3,27 +3,15 @@ import { FormData } from '@/types/rank-calculator';
 import { useWatch } from 'react-hook-form';
 
 export function usePointCalculator() {
-  const totalLevel = useWatch<FormData, 'totalLevel'>({
-    name: 'totalLevel',
-  });
-  const joinDate = useWatch<FormData, 'joinDate'>({
-    name: 'joinDate',
-  });
-  const items = useWatch<FormData, 'items'>({
-    name: 'items',
-  });
-  const ehp = useWatch<FormData, 'ehp'>({
-    name: 'ehp',
-  });
-  const ehb = useWatch<FormData, 'ehb'>({
-    name: 'ehb',
-  });
+  const totalLevel = useWatch<FormData, 'totalLevel'>({ name: 'totalLevel' });
+  const joinDate = useWatch<FormData, 'joinDate'>({ name: 'joinDate' });
+  const items = useWatch<FormData, 'items'>({ name: 'items' });
+  const ehp = useWatch<FormData, 'ehp'>({ name: 'ehp' });
+  const ehb = useWatch<FormData, 'ehb'>({ name: 'ehb' });
   const achievementDiaries = useWatch<FormData, 'achievementDiaries'>({
     name: 'achievementDiaries',
   });
-  const caTier = useWatch<FormData, 'caTier'>({
-    name: 'caTier',
-  });
+  const caTier = useWatch<FormData, 'caTier'>({ name: 'caTier' });
 
   console.log({
     totalLevel,
