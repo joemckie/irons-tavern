67aa93099419354426ec1344d2214084672ab558
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-24 15:59:31 +0100

    chore: more defaults

--- app/rank-calculator/hooks/point-calculator/use-rank-calculator.ts
+++ app/rank-calculator/hooks/point-calculator/use-rank-calculator.ts
@@ -1,3 +1,4 @@
+import { Rank } from '@/config/enums';
 import { CategoryPointCalculatorData } from '@/types/rank-calculator';
 
 export function useRankCalculator() {
@@ -5,7 +6,7 @@ export function useRankCalculator() {
     pointsAwarded: 0,
     pointsAwardedPercentage: 0,
     availablePoints: 0,
-    rank: 'Warlock',
-    nextRank: 'Dragonstone',
+    rank: Rank.Air,
+    nextRank: Rank.Pine,
   } satisfies CategoryPointCalculatorData;
 }
