c8737be74140f4239ec8c33b60f030c5ce6925e4
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-25 12:25:31 +0100

    chore: add jest

--- app/rank-calculator/hooks/point-calculator/use-calculator-scaling.ts
+++ app/rank-calculator/hooks/point-calculator/use-calculator-scaling.ts
@@ -1,3 +1,10 @@
+import { FormData } from '@/types/rank-calculator';
+import { useWatch } from 'react-hook-form';
+
 export function useCalculatorScaling() {
+  const joinDate = useWatch<FormData, 'joinDate'>({ name: 'joinDate' });
+
+  console.log({ joinDate });
+
   return 100;
 }
