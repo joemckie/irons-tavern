7374f87edc60badc44a3cffa1eaef5c15a7a685d
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-25 14:05:12 +0100

    chore: offset days joined by 1

--- app/rank-calculator/utils/calculate-scaling.ts
+++ app/rank-calculator/utils/calculate-scaling.ts
@@ -6,7 +6,9 @@ export function calculateScaling(joinDate: Date | null) {
   const width = 15;
   const minScaling = 0.1;
   const scalingStep = (width / maxScalingAfterXDays) * (1 - minScaling);
-  const daysSinceJoined = differenceInDays(today, joinDate ?? new Date());
+  const dayOffset = 1; // We count the first day in clan as day 1
+  const daysSinceJoined =
+    differenceInDays(today, joinDate ?? new Date()) + dayOffset;
   const unclippedScaling = Number(
     (Math.floor(daysSinceJoined / width) * scalingStep + minScaling).toFixed(3),
   );
