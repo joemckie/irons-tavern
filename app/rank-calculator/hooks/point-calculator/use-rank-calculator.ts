b1bf90cda2663b9243450c1085876c147ac87d1c
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-29 09:42:59 +0000

    chore: hook in total point calculations

--- app/rank-calculator/hooks/point-calculator/use-rank-calculator.ts
+++ app/rank-calculator/hooks/point-calculator/use-rank-calculator.ts
@@ -1,5 +1,7 @@
 import { Rank } from '@/config/enums';
 import { CommonPointCalculatorData } from '@/types/rank-calculator';
+import { useCollectionLogPointCalculator } from './use-collection-log-point-calculator';
+import { useNotableItemsPointCalculator } from './use-notable-items-point-calculator';
 
 export interface RankCalculatorData extends CommonPointCalculatorData {
   rank: Rank;
@@ -7,8 +9,14 @@ export interface RankCalculatorData extends CommonPointCalculatorData {
 }
 
 export function useRankCalculator() {
+  const { pointsAwarded: totalCollectionLogPoints } =
+    useCollectionLogPointCalculator();
+  const { pointsAwarded: totalNotableItemsPoints } =
+    useNotableItemsPointCalculator();
+  const pointsAwarded = totalCollectionLogPoints + totalNotableItemsPoints;
+
   return {
-    pointsAwarded: 0,
+    pointsAwarded,
     pointsAwardedPercentage: 0,
     pointsRemaining: 0,
     rank: Rank.Air,
