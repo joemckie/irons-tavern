515553abf8b5b93a53aabb857447406f439f0d0e
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-30 00:47:13 +0000

    feat: scale item points in list

--- app/rank-calculator/components/item.tsx
+++ app/rank-calculator/components/item.tsx
@@ -4,30 +4,44 @@ import { Item } from '@/types/items';
 import { Checkbox } from './checkbox';
 import { stripEntityName } from '../utils/strip-entity-name';
 import { EntityImage } from './entity-image';
+import { useCalculatorScaling } from '../hooks/point-calculator/use-calculator-scaling';
 
 interface ItemProps {
   acquired: boolean;
   item: Item;
 }
 
-export const MemoisedItem = memo(({ item, acquired }: ItemProps) => (
-  <Table.Row key={item.name} align="center">
-    <Table.Cell>
-      <Flex align="center" gap="2">
-        <EntityImage alt={`${item.name} icon`} src={item.image} fallback="?" />
-        <Text>{item.name}</Text>
-      </Flex>
-    </Table.Cell>
-    <Table.Cell align="right">
-      <Checkbox
-        defaultChecked={acquired}
-        name={`acquiredItems.${stripEntityName(item.name)}`}
-      />
-    </Table.Cell>
-    <Table.Cell aria-label={`${item.name} points`} align="right" width="100px">
-      {`${acquired ? item.points : 0} / ${item.points}`}
-    </Table.Cell>
-  </Table.Row>
-));
+export const MemoisedItem = memo(({ item, acquired }: ItemProps) => {
+  const scaling = useCalculatorScaling();
+  const scaledItemPoints = Math.floor(item.points * scaling);
+
+  return (
+    <Table.Row key={item.name} align="center">
+      <Table.Cell>
+        <Flex align="center" gap="2">
+          <EntityImage
+            alt={`${item.name} icon`}
+            src={item.image}
+            fallback="?"
+          />
+          <Text>{item.name}</Text>
+        </Flex>
+      </Table.Cell>
+      <Table.Cell align="right">
+        <Checkbox
+          defaultChecked={acquired}
+          name={`acquiredItems.${stripEntityName(item.name)}`}
+        />
+      </Table.Cell>
+      <Table.Cell
+        aria-label={`${item.name} points`}
+        align="right"
+        width="100px"
+      >
+        {`${acquired ? scaledItemPoints : 0} / ${scaledItemPoints}`}
+      </Table.Cell>
+    </Table.Row>
+  );
+});
 
 MemoisedItem.displayName = 'MemoisedItem';
