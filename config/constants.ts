2be9fa27d35a0665b721e9b822939a4cadb5ca9f
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-10-30 14:12:40 +0000

    chore: add discord packages

--- config/constants.ts
+++ config/constants.ts
@@ -25,4 +25,5 @@ export const constants = {
     baseUrl: 'https://sync.runescape.wiki' as const,
   },
   redisUrl: process.env.KV_REST_API_URL,
+  discordUrl: 'https://discord.com/api' as const,
 };
