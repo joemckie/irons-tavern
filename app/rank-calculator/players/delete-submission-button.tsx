e6fad164d3be2ffbeff4e50b83f4d0cf85308f06
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-11-02 13:31:53 +0000

    feat: add delete modal

--- app/rank-calculator/players/delete-submission-button.tsx
+++ app/rank-calculator/players/delete-submission-button.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { TrashIcon } from '@radix-ui/react-icons';
-import { IconButton } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex, IconButton, Text } from '@radix-ui/themes';
 import { useCallback } from 'react';
 import { deletePlayerAccount } from '../actions/player-accounts';
 
@@ -17,8 +17,33 @@ export function DeleteSubmissionButton({
   }, [playerName]);
 
   return (
-    <IconButton color="red" variant="soft" onClick={handleDelete}>
-      <TrashIcon />
-    </IconButton>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <IconButton color="red" variant="soft">
+          <TrashIcon />
+        </IconButton>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Remove account</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          <Text>
+            Are you sure? {playerName} and any associated submissions will be
+            deleted.
+          </Text>
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red" onClick={handleDelete}>
+              Remove account
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 }
