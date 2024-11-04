43dfcba0e963395a44426ae3cd99945df1581c94
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-11-04 01:39:52 +0000

    fix: mobile styling flickering

--- app/rank-calculator/[player]/rank-calculator.tsx
+++ app/rank-calculator/[player]/rank-calculator.tsx
@@ -1,7 +1,6 @@
 import { Grid } from '@radix-ui/themes';
 import { SubmitHandler, useFormContext } from 'react-hook-form';
 import { FormData } from '@/types/rank-calculator';
-import { usePageLayout } from '../hooks/use-page-layout';
 import { Navigation } from '../components/navigation';
 import { Sidebar } from '../components/sidebar';
 import { RightSidebar } from '../components/right-sidebar';
@@ -13,23 +12,33 @@ interface RankCalculatorProps {
 
 export function RankCalculator({ onSubmit }: RankCalculatorProps) {
   const { handleSubmit } = useFormContext<FormData>();
-  const { navHeight, navRef } = usePageLayout();
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Grid
-        areas="
-        'nav nav nav'
-        'sidebar main right-sidebar'
-        "
-        columns="
-        [sidebar] minmax(200px, 1fr)
-        [main] minmax(0, 2fr)
-        [right-sidebar] minmax(200px, 1fr)
-        "
-        rows={`${navHeight}px 1fr`}
+        areas={{
+          initial: `
+            'nav'
+            'sidebar'
+            'right-sidebar'
+            'main'
+          `,
+          md: `
+            'nav nav nav'
+            'sidebar main right-sidebar'
+          `,
+        }}
+        columns={{
+          initial: 'repeat(auto-fit, minmax(400px, 1fr))',
+          md: `
+            [sidebar] minmax(200px, 1fr)
+            [main] minmax(0, 2fr)
+            [right-sidebar] minmax(200px, 1fr)
+        `,
+        }}
+        rows="54px 1fr"
       >
-        <Navigation ref={navRef} />
+        <Navigation />
         <Sidebar />
         <RightSidebar />
         <ItemList />
