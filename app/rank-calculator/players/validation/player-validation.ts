0485d89c89f9e6158495506c250c81ad0b150731
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-11-11 00:39:14 +0000

    feat: prevent API errors from incorrectly flagging invalid users

--- app/rank-calculator/players/validation/player-validation.ts
+++ app/rank-calculator/players/validation/player-validation.ts
@@ -1,15 +1,23 @@
 'use server';
 
+import { StatusCodes } from 'http-status-codes';
+
 export async function validatePlayerExists(playerName: string) {
   'use server';
 
-  try {
-    const response = await fetch(
-      `https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=${playerName}`,
-    );
+  const response = await fetch(
+    `https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=${playerName}`,
+  );
 
-    return response.status === 200;
-  } catch {
-    return false;
+  if (
+    [StatusCodes.TOO_MANY_REQUESTS, StatusCodes.INTERNAL_SERVER_ERROR].includes(
+      response.status,
+    )
+  ) {
+    // Bail early if the query is unhealthy, as this doesn't
+    // mean the player name is non-existent
+    throw new Error('Unable to query the OSRS player API');
   }
+
+  return response.status === StatusCodes.OK;
 }
