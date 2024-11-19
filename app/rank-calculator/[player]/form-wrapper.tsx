d4ae2abe78284a0b86aac8364156863f07bdc1ac
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-11-19 21:45:40 +0000

    refactor: form toast notifications

--- app/rank-calculator/[player]/form-wrapper.tsx
+++ app/rank-calculator/[player]/form-wrapper.tsx
@@ -3,13 +3,13 @@
 import { FormProvider } from 'react-hook-form';
 import { useHookFormAction } from '@next-safe-action/adapter-react-hook-form/hooks';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { toast } from 'react-toastify';
 import { Rank } from '@/config/enums';
 import { RankCalculator } from './rank-calculator';
 import { RankCalculatorSchema } from './submit-rank-calculator-validation';
 import { RankCalculatorNavigationActions } from '../components/rank-calculator-navigation-actions';
 import { Navigation } from '../components/navigation';
 import { saveDraftRankSubmissionAction } from './actions/save-draft-rank-submission-action';
+import { actionToastMessage } from '../utils/action-toast-message';
 
 interface FormWrapperProps {
   formData: Omit<RankCalculatorSchema, 'rank' | 'points'>;
@@ -17,34 +17,42 @@ interface FormWrapperProps {
 }
 
 export function FormWrapper({ formData, currentRank }: FormWrapperProps) {
-  const { handleSubmitWithAction, form } = useHookFormAction(
+  const {
+    form,
+    action: { executeAsync: saveDraftRankSubmission },
+  } = useHookFormAction(
     saveDraftRankSubmissionAction,
     zodResolver(RankCalculatorSchema),
     {
-      actionProps: {
-        onSuccess({ input }) {
-          toast.success('Draft saved!');
-
-          form.reset(input);
-        },
-        onError({ error: { serverError } }) {
-          if (serverError) {
-            toast.error(serverError);
-          }
-        },
-      },
       formProps: {
         defaultValues: formData,
         criteriaMode: 'all',
         mode: 'onBlur',
+        resetOptions: {
+          keepIsSubmitted: true,
+        },
       },
     },
   );
+  const { handleSubmit } = form;
 
   return (
     <FormProvider {...form}>
       <RankCalculator
-        submitRankCalculatorAction={handleSubmitWithAction}
+        submitRankCalculatorAction={(e) => {
+          e.preventDefault();
+
+          actionToastMessage(handleSubmit(saveDraftRankSubmission)(e), {
+            pending: 'Saving draft...',
+            success: {
+              render() {
+                form.reset(form.getValues());
+
+                return 'Draft saved!';
+              },
+            },
+          });
+        }}
         navigation={
           <Navigation
             actions={
