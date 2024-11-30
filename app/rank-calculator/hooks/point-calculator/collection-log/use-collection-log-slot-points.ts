2ce375b27720094aa6b3bc40f0d1edca9f64b0c9
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-11-30 16:40:57 +0000

    refactor: collection log card

--- app/rank-calculator/hooks/point-calculator/collection-log/use-collection-log-slot-points.ts
+++ app/rank-calculator/hooks/point-calculator/collection-log/use-collection-log-slot-points.ts
@@ -1,6 +1,6 @@
 import { RankCalculatorSchema } from '@/app/rank-calculator/[player]/submit-rank-calculator-validation';
 import { useWatch } from 'react-hook-form';
-import { calculatePointsForLogSlots } from './calculate-points-for-log-slots';
+import { calculatePointsForLogSlots } from '../../../utils/calculators/calculate-points-for-log-slots';
 import { useCalculatorScaling } from '../use-calculator-scaling';
 
 export function useCollectionLogSlotPoints() {
