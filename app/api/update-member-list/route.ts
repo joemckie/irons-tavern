e25029c57d48eab0b7ee854da7491e2c87e071de
Joe McKie <4980618+joemckie@users.noreply.github.com>
2024-12-20 17:44:14 +0000

    Add option to skip temple update when updating members

--- app/api/update-member-list/route.ts
+++ app/api/update-member-list/route.ts
@@ -17,7 +17,9 @@ interface ClanExport {
 }
 
 export async function POST(request: NextRequest) {
+  const updateTemple = request.nextUrl.searchParams.get('updateTemple');
   const body: ClanExport = await request.json();
+  
   const { members, leaders } = body.clanMemberMaps.reduce(
     (acc, member) => {
       if (clientConstants.ranks.leaders.includes(member.rank)) {
@@ -46,9 +48,9 @@ export async function POST(request: NextRequest) {
     name: serverConstants.temple.groupName,
     leaders: leaders.toString(),
     members: members.toString(),
-    ...(serverConstants.temple.privateGroup && {
+    ...(serverConstants.temple.privateGroup ? {
       'private-group-checkbox': 'on',
-    }),
+    } : {}),
   } satisfies GroupUpdateRequest;
 
   console.log('Updating member list');
@@ -64,8 +66,10 @@ export async function POST(request: NextRequest) {
     put('members.json', JSON.stringify(body.clanMemberMaps), {
       access: 'public',
     }),
-    // Check all players in the new member list
-    fetch(`${clientConstants.publicUrl}/api/check-all-players`),
+    ...(updateTemple ? [
+      // Check all players in the new member list
+      fetch(`${clientConstants.publicUrl}/api/check-all-players`),
+    ] : [])
   ]);
 
   return NextResponse.json({ success: true });
