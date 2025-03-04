156185bcecc6c805c1b6443326514f8c8dca379f
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-03-04 21:11:21 +0000

    feat: add FAQ link to nav bar

--- app/rank-calculator/components/rank-calculator-navigation-actions.tsx
+++ app/rank-calculator/components/rank-calculator-navigation-actions.tsx
@@ -42,82 +42,92 @@ export function RankCalculatorNavigationActions({
   );
 
   return (
-    <Flex>
-      <Button
-        role="button"
-        loading={isSubmitting || isActionActive}
-        disabled={!isDirty || !isValid || isSubmitting || isActionActive}
-        variant="soft"
-        type="submit"
-        style={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
-      >
-        Save
+    <Flex gap="1">
+      <Button asChild variant="soft" color="gray" type="button">
+        <Link
+          href="https://discordapp.com/channels/697956179854229552/1346496963331358787"
+          target="_blank"
+        >
+          Help
+        </Link>
       </Button>
-      <DropdownMenu.Root modal={false}>
-        <DropdownMenu.Trigger disabled={isSubmitting || isActionActive}>
-          <IconButton
-            className="save-dropdown-button"
-            variant="soft"
-            type="button"
-            style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
-          >
-            <ChevronDownIcon />
-          </IconButton>
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Content color="gray" variant="soft">
-          <DropdownMenu.Item
-            onClick={() => {
-              if (isDirty) {
-                toast.error('Please save your data first!');
-
-                return;
-              }
-
-              handleToastUpdates(
-                publishRankSubmission({
-                  totalPoints,
-                  rank,
-                }),
-                { success: 'Rank application submitted!' },
-              );
-            }}
-          >
-            Apply for promotion
-          </DropdownMenu.Item>
-          <DropdownMenu.Item
-            disabled={!isDirty}
-            onClick={() => {
-              startResetTransition(() => {
-                reset();
-              });
-            }}
-          >
-            Reset form defaults
-          </DropdownMenu.Item>
-          <DropdownMenu.Item asChild>
-            <Link
-              href={`/rank-calculator/players/edit/${playerName.toLowerCase()}`}
+      <Flex>
+        <Button
+          role="button"
+          loading={isSubmitting || isActionActive}
+          disabled={!isDirty || !isValid || isSubmitting || isActionActive}
+          variant="soft"
+          type="submit"
+          style={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
+        >
+          Save
+        </Button>
+        <DropdownMenu.Root modal={false}>
+          <DropdownMenu.Trigger disabled={isSubmitting || isActionActive}>
+            <IconButton
+              className="save-dropdown-button"
+              variant="soft"
+              type="button"
+              style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
             >
-              Edit player
-            </Link>
-          </DropdownMenu.Item>
-          <DropdownMenu.Separator />
-          <DropdownMenu.Item
-            color="red"
-            onSelect={() => {
-              startDeleteDialogTransition(() => {
-                setIsDeleteSubmissionDataDialogOpen(true);
-              });
-            }}
-          >
-            Delete data
-          </DropdownMenu.Item>
-        </DropdownMenu.Content>
-      </DropdownMenu.Root>
-      <DeleteSubmissionDataDialog
-        open={isDeleteSubmissionDataDialogOpen}
-        onOpenChange={setIsDeleteSubmissionDataDialogOpen}
-      />
+              <ChevronDownIcon />
+            </IconButton>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content color="gray" variant="soft">
+            <DropdownMenu.Item
+              onClick={() => {
+                if (isDirty) {
+                  toast.error('Please save your data first!');
+
+                  return;
+                }
+
+                handleToastUpdates(
+                  publishRankSubmission({
+                    totalPoints,
+                    rank,
+                  }),
+                  { success: 'Rank application submitted!' },
+                );
+              }}
+            >
+              Apply for promotion
+            </DropdownMenu.Item>
+            <DropdownMenu.Item
+              disabled={!isDirty}
+              onClick={() => {
+                startResetTransition(() => {
+                  reset();
+                });
+              }}
+            >
+              Reset form defaults
+            </DropdownMenu.Item>
+            <DropdownMenu.Item asChild>
+              <Link
+                href={`/rank-calculator/players/edit/${playerName.toLowerCase()}`}
+              >
+                Edit player
+              </Link>
+            </DropdownMenu.Item>
+            <DropdownMenu.Separator />
+            <DropdownMenu.Item
+              color="red"
+              onSelect={() => {
+                startDeleteDialogTransition(() => {
+                  setIsDeleteSubmissionDataDialogOpen(true);
+                });
+              }}
+            >
+              Delete data
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+        <DeleteSubmissionDataDialog
+          open={isDeleteSubmissionDataDialogOpen}
+          onOpenChange={setIsDeleteSubmissionDataDialogOpen}
+        />
+      </Flex>
     </Flex>
   );
 }
