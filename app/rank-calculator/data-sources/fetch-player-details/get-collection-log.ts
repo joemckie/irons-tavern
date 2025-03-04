32e76579b541fcc3fc032d2b8d732665454ef598
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-03-04 16:46:17 +0000

    fix: handle unreponsive collection log requests

--- app/rank-calculator/data-sources/fetch-player-details/get-collection-log.ts
+++ app/rank-calculator/data-sources/fetch-player-details/get-collection-log.ts
@@ -1,6 +1,7 @@
 import { clientConstants } from '@/config/constants.client';
 import { CollectionLogResponse } from '@/app/schemas/collection-log';
 import * as Sentry from '@sentry/nextjs';
+import { ZodError } from 'zod';
 
 export async function getCollectionLog(player: string) {
   try {
@@ -8,9 +9,22 @@ export async function getCollectionLog(player: string) {
       `${clientConstants.collectionLog.baseUrl}/collectionlog/user/${player}`,
     );
 
+    if (!collectionLogResponse.ok) {
+      throw new Error('collectionlog.net did not respond');
+    }
+
     return CollectionLogResponse.parse(await collectionLogResponse.json());
-  } catch {
-    Sentry.captureMessage('Collection Log data not found', 'info');
+  } catch (e) {
+    if (e instanceof ZodError) {
+      Sentry.captureMessage('Collection Log data not found', 'info');
+    }
+
+    if (
+      e instanceof Error &&
+      e.message === 'collectionlog.net did not respond'
+    ) {
+      Sentry.captureMessage(e.message, 'info');
+    }
 
     return null;
   }
