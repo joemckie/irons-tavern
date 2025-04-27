7b85e92d9e4f0fcc8859033446593d92e380ae0d
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-04-27 22:58:15 +0100

    fix: types

--- app/rank-calculator/hooks/point-calculator/notable-items/use-max-notable-items-points.ts
+++ app/rank-calculator/hooks/point-calculator/notable-items/use-max-notable-items-points.ts
@@ -1,8 +1,10 @@
 import { calculateMaximumNotableItemsPoints } from '@/app/rank-calculator/utils/calculators/calculate-maximum-notable-items-points';
 import { useCalculatorScaling } from '../use-calculator-scaling';
+import { useDropRates } from '../../use-drop-rates';
 
 export function useMaxNotableItemsPoints() {
   const scaling = useCalculatorScaling();
+  const { data: dropRates } = useDropRates();
 
-  return calculateMaximumNotableItemsPoints(scaling);
+  return calculateMaximumNotableItemsPoints(dropRates, scaling);
 }
