6d57da55b40b573337af1063db64da3d12b585a5
Turbobot <turbobot@vercel.com>
2025-05-11 11:54:30 +0100

    feat(create-turbo): apply official-starter transform

---
+++ app/api/sync/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  console.log(await request.json());
+
+  return NextResponse.json({ success: true });
+}
