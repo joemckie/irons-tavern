132e6ee24f3e43fb67c0b1f5aa6283e0b1ceac06
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-05-20 01:15:14 +0100

    feat: cache wiki combat achievement requests

--- apps/web/app/rank-calculator/data-sources/fetch-player-details/get-combat-achievement-id-map.ts
+++ apps/web/app/rank-calculator/data-sources/fetch-player-details/get-combat-achievement-id-map.ts
@@ -5,51 +5,58 @@ import {
   CombatAchievementListResponse,
 } from '@/app/schemas/wiki';
 import * as Sentry from '@sentry/nextjs';
+import { unstable_cache } from 'next/cache';
 
-export async function getCaIdMap() {
-  const query = [
-    '[[Category:Easy Combat Achievements tasks||Medium Combat Achievements tasks||Hard Combat Achievements tasks||Elite Combat Achievements tasks||Master Combat Achievements tasks||Grandmaster Combat Achievements tasks]]',
-    '?Combat Achievement JSON',
-    'limit=1000',
-  ].join('|');
+export const getCaIdMap = unstable_cache(
+  async () => {
+    const query = [
+      '[[Category:Easy Combat Achievements tasks||Medium Combat Achievements tasks||Hard Combat Achievements tasks||Elite Combat Achievements tasks||Master Combat Achievements tasks||Grandmaster Combat Achievements tasks]]',
+      '?Combat Achievement JSON',
+      'limit=1000',
+    ].join('|');
 
-  const params = new URLSearchParams({
-    action: 'ask',
-    format: 'json',
-    query,
-    api_version: '2',
-    formatversion: '2',
-  });
+    const params = new URLSearchParams({
+      action: 'ask',
+      format: 'json',
+      query,
+      api_version: '2',
+      formatversion: '2',
+    });
 
-  try {
-    const allCombatAchievementsResponse = await fetch(
-      `${clientConstants.wiki.baseUrl}/api.php?${params}`,
-    );
+    try {
+      const allCombatAchievementsResponse = await fetch(
+        `${clientConstants.wiki.baseUrl}/api.php?${params}`,
+      );
 
-    const data = CombatAchievementListResponse.parse(
-      await allCombatAchievementsResponse.json(),
-    );
+      const data = CombatAchievementListResponse.parse(
+        await allCombatAchievementsResponse.json(),
+      );
 
-    return Object.values(data.query.results).reduce<Record<string, number>>(
-      (acc, val) => {
-        const [combatAchievementJson] =
-          val.printouts['Combat Achievement JSON'];
+      return Object.values(data.query.results).reduce<Record<string, number>>(
+        (acc, val) => {
+          const [combatAchievementJson] =
+            val.printouts['Combat Achievement JSON'];
 
-        if (!combatAchievementJson) {
-          return acc;
-        }
+          if (!combatAchievementJson) {
+            return acc;
+          }
 
-        const { id, tier } = CombatAchievementJson.parse(
-          JSON.parse(combatAchievementJson),
-        );
+          const { id, tier } = CombatAchievementJson.parse(
+            JSON.parse(combatAchievementJson),
+          );
 
-        return { ...acc, [id]: combatAchievementTierPoints[tier] };
-      },
-      {},
-    );
-  } catch (error) {
-    Sentry.captureException(error);
+          return { ...acc, [id]: combatAchievementTierPoints[tier] };
+        },
+        {},
+      );
+    } catch (error) {
+      Sentry.captureException(error);
 
-    return null;
-  }
-}
+      return null;
+    }
+  },
+  [],
+  {
+    revalidate: 60 * 60 * 24 * 7, // 7 days
+  },
+);
