6a65bea7c64d0f70b933152c9a42619e47180ff0
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-05-20 01:15:14 +0100

    feat: cache wiki combat achievement requests

--- apps/web/app/rank-calculator/data-sources/fetch-player-details/get-combat-achievement-tier-thresholds.ts
+++ apps/web/app/rank-calculator/data-sources/fetch-player-details/get-combat-achievement-tier-thresholds.ts
@@ -2,57 +2,64 @@ import { clientConstants } from '@/config/constants.client';
 import { CombatAchievementTier } from '@/app/schemas/osrs';
 import { CombatAchievementTierThresholdResponse } from '@/app/schemas/wiki';
 import * as Sentry from '@sentry/nextjs';
+import { unstable_cache } from 'next/cache';
 
-export async function getCombatAchievementTierThresholds(): Promise<Record<
-  Exclude<CombatAchievementTier, 'None'>,
-  number
-> | null> {
-  const query = [
-    '{{Globals|ca easy points}}',
-    '{{Globals|ca medium points}}',
-    '{{Globals|ca hard points}}',
-    '{{Globals|ca elite points}}',
-    '{{Globals|ca master points}}',
-    '{{Globals|ca gm points}}',
-  ].join('|');
+export const getCombatAchievementTierThresholds = unstable_cache(
+  async (): Promise<Record<
+    Exclude<CombatAchievementTier, 'None'>,
+    number
+  > | null> => {
+    const query = [
+      '{{Globals|ca easy points}}',
+      '{{Globals|ca medium points}}',
+      '{{Globals|ca hard points}}',
+      '{{Globals|ca elite points}}',
+      '{{Globals|ca master points}}',
+      '{{Globals|ca gm points}}',
+    ].join('|');
 
-  const tierPointThresholdParams = new URLSearchParams({
-    action: 'expandtemplates',
-    format: 'json',
-    text: query,
-    prop: 'wikitext',
-    formatversion: '2',
-  }).toString();
+    const tierPointThresholdParams = new URLSearchParams({
+      action: 'expandtemplates',
+      format: 'json',
+      text: query,
+      prop: 'wikitext',
+      formatversion: '2',
+    }).toString();
 
-  try {
-    const response = await fetch(
-      `${clientConstants.wiki.baseUrl}/api.php?${tierPointThresholdParams}`,
-    );
+    try {
+      const response = await fetch(
+        `${clientConstants.wiki.baseUrl}/api.php?${tierPointThresholdParams}`,
+      );
 
-    const data = CombatAchievementTierThresholdResponse.parse(
-      await response.json(),
-    );
+      const data = CombatAchievementTierThresholdResponse.parse(
+        await response.json(),
+      );
 
-    const [
-      easyPoints,
-      mediumPoints,
-      hardPoints,
-      elitePoints,
-      masterPoints,
-      gmPoints,
-    ] = data.expandtemplates.wikitext.split('|');
+      const [
+        easyPoints,
+        mediumPoints,
+        hardPoints,
+        elitePoints,
+        masterPoints,
+        gmPoints,
+      ] = data.expandtemplates.wikitext.split('|');
 
-    return {
-      Easy: Number(easyPoints),
-      Medium: Number(mediumPoints),
-      Hard: Number(hardPoints),
-      Elite: Number(elitePoints),
-      Master: Number(masterPoints),
-      Grandmaster: Number(gmPoints),
-    };
-  } catch (error) {
-    Sentry.captureException(error);
+      return {
+        Easy: Number(easyPoints),
+        Medium: Number(mediumPoints),
+        Hard: Number(hardPoints),
+        Elite: Number(elitePoints),
+        Master: Number(masterPoints),
+        Grandmaster: Number(gmPoints),
+      };
+    } catch (error) {
+      Sentry.captureException(error);
 
-    return null;
-  }
-}
+      return null;
+    }
+  },
+  [],
+  {
+    revalidate: 60 * 60 * 24 * 7, // 7 days
+  },
+);
