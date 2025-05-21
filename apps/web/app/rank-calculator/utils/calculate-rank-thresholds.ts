92eb8b872639607787a6d91ac6828c55ecadda5a
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-05-21 15:11:59 +0100

    feat: update ranks (#77)
    * feat: update ranks
    
    * fix: build
    
    * chore: change legacy rank
    
    * fix: roles
    
    * fix: update rank proportions
    
    * fix: update rank thresholds
    
    * chore: reorder
    
    * feat: add beast role id

--- apps/web/app/rank-calculator/utils/calculate-rank-thresholds.ts
+++ apps/web/app/rank-calculator/utils/calculate-rank-thresholds.ts
@@ -18,19 +18,19 @@ export const calculateRankThresholds = (
 
       return acc;
     },
-    {} as Record<Exclude<StandardRank, 'Air' | 'Pine'>, number>,
+    {} as Record<Exclude<StandardRank, 'Helper' | 'Steel'>, number>,
   );
 
   return {
-    Standard: { Air: 0, Pine: 1, ...standardRankPoints },
-    Legacy: { Legend: 0 },
+    Standard: { Helper: 0, Steel: 1, ...standardRankPoints },
+    Legacy: { Legacy: 0 },
     Admin: {
       Captain: 0,
-      General: standardRankPoints.Nature,
-      Colonel: standardRankPoints.Yew,
-      Brigadier: standardRankPoints.Elite,
-      Admiral: standardRankPoints.Diseased,
-      Marshal: standardRankPoints.Blisterwood,
+      General: standardRankPoints.Striker,
+      Colonel: standardRankPoints.Legend,
+      Brigadier: standardRankPoints.Sage,
+      Admiral: standardRankPoints.Skulled,
+      Marshal: standardRankPoints.Beast,
     },
     Moderator: { Moderator: 0 },
     'Deputy Owner': { 'Deputy Owner': 0 },
