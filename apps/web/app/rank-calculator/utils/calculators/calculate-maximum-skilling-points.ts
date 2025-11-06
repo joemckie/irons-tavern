da412fac7aafac0f15ee1d73d5268028a694b342
Joe McKie <4980618+joemckie@users.noreply.github.com>
2025-11-06 19:53:51 +0000

    Remove outdated comments from calculateMaximumSkillingPoints
    Removed comments about sailing offset in the calculateMaximumSkillingPoints function.

--- apps/web/app/rank-calculator/utils/calculators/calculate-maximum-skilling-points.ts
+++ apps/web/app/rank-calculator/utils/calculators/calculate-maximum-skilling-points.ts
@@ -1,8 +1,6 @@
 import { pointsConfig } from '../../config/points';
 
 export function calculateMaximumSkillingPoints(scaling: number) {
-  // Remove sailing offset when Sailing is released
-  // It is used to make max points equal 100% pre-Sailing
   const {
     maxCapePoints,
     achievementDiaryCapePoints,
