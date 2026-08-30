f5220d5bd6ad752ff841c3e6a8f45d703641e087
Joe McKie <4980618+joemckie@users.noreply.github.com>
2026-08-31 00:48:16 +0100

    Update the-nightmare.ts

--- apps/web/data/item-categories/the-nightmare.ts
+++ apps/web/data/item-categories/the-nightmare.ts
@@ -3,7 +3,7 @@ import { ItemCategory } from '@/app/schemas/items';
 import { singleItem } from '../utils/item-builders';
 
 export const theNightmare: ItemCategory = {
-  image: formatWikiImageUrl('Little nightmare chathead', 'category'),
+  image: formatWikiImageUrl('Little Nightmare chathead', 'category'),
   items: [
     singleItem({
       name: 'Nightmare staff',
